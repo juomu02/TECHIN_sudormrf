58842a4ac98e79398c2746190d7e4d5799922769
juozas <muleviciusj@gmail.com>
2026-05-14 15:32:49 +0300

    small changes to endpoint names

--- MySavings.API/Controllers/UserController.cs
+++ MySavings.API/Controllers/UserController.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using MySavings.Services;
 
 namespace MySavings.API.Controllers
@@ -19,7 +14,7 @@ namespace MySavings.API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
+        public async Task<IActionResult> GetAsync(int id)
         {
             var user = await userService.GetAsync(id);
             if (user == null)
@@ -29,7 +24,7 @@ namespace MySavings.API.Controllers
             return Ok(user);
         }
         [HttpPost("{userName}/{email}/{password}")]
-        public async Task<IActionResult> Post(string userName, string email, string password)
+        public async Task<IActionResult> CreateAsync(string userName, string email, string password)
         {
             var userId = await userService.AddAsync(userName, email, password);
             return Ok(userId);
